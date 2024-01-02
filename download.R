6a11ec430594afb64a6ae2499e9759921fe833a5
Jeroen Ooms <jeroenooms@gmail.com>
2024-01-02 16:03:06 +0100

    Test popular packages

--- download.R
+++ download.R
@@ -1,24 +1,22 @@
-# Test set of interesting packages
-pkgs <- c("antiword", "archive", "arrangements", "arrow", "av", "brotli",
-"Cairo", "cld2", "curl", "data.table", "duckdb", "fftw", "fftwtools",
-"gam", "gdtools", "gert", "git2r", "glmnet", "gmp", "httpgd", "httpuv",
-"hunspell", "igraph", "image.textlinedetector", "imager", "jqr",
-"lwgeom", "magick", "Matrix", "mongolite", "odbc", "opencv",
-"openssl", "pdftools", "proj4", "protolite", "ragg", "RcppAlgos",
-"RcppParallel", "redux", "RMariaDB", "RMySQL", "RPostgres", "rsvg",
-"rzmq", "sf", "shiny", "sodium", "sparsenet", "ssh", "stars",
-"stringi", "terra", "tesseract", "tidyverse", "tiledb", "unrtf",
-"V8", "webp", "writexl")
+# Get popular CRAN packages
+options(repos = c(CRAN='https://cloud.r-project.org'))
+deps <- tools::package_dependencies(reverse = TRUE)
+popular <- names(deps)[order(sapply(deps, length), decreasing = TRUE)]
+avail <- as.data.frame(available.packages())
+srcpkgs <- avail[avail$NeedsCompilation == 'yes', 'Package']
+pkgs <- sort(intersect(srcpkgs, head(popular, 1000)))
+
+# Allow bioc dependencies
+setRepositories(ind=1:4)
 
 # Install binaries + dependencies
 cat("::group::Installing host dependencies\n")
-install.packages(pkgs, repos = 'https://cloud.r-project.org')
+Sys.setenv(R_COMPILE_AND_INSTALL_PACKAGES='never')
+install.packages(pkgs, type = 'binary', quiet = TRUE)
 cat("::endgroup::\n")
 
 # Get all the sources
 cat("::group::Downloading sources\n")
-deps <- unname(unlist(tools::package_dependencies(pkgs, recursive = TRUE)))
-allpkgs <- sort(unique(c(pkgs, deps)))
 dir.create('sources')
-download.packages(allpkgs, 'sources', repos = 'https://cloud.r-project.org')
+download.packages(pkgs, 'sources', quiet = TRUE)
 cat("::endgroup::\n")
